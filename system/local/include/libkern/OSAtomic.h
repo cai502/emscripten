3ed914ead8762d6701969682655ae90632e8203d
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2015-05-12 18:31:22 +0900

    complete OSAtomic.h implementation

--- system/local/include/libkern/OSAtomic.h
+++ system/local/include/libkern/OSAtomic.h
@@ -1,67 +1,70 @@
 #ifndef __OSAtomic
 #define __OSAtomic
 
-__inline BOOL OSAtomicCompareAndSwapLong(long oldl, long newl, long volatile *dst)
+#include <stdbool.h>
+#include <stdint.h>
+
+static __inline bool OSAtomicCompareAndSwapLong(long oldl, long newl, long volatile *dst)
 {
   return __sync_bool_compare_and_swap(dst, oldl, newl);
 }
 
-__inline BOOL OSAtomicCompareAndSwapPtr(void *oldp, void *newp, void * volatile *dst)
+static __inline bool OSAtomicCompareAndSwapPtr(void *oldp, void *newp, void * volatile *dst)
 {
   return __sync_bool_compare_and_swap(dst, oldp, newp);
 }
 
-__inline BOOL OSAtomicCompareAndSwapPtrBarrier(void *oldp, void *newp, void * volatile *dst)
+static __inline bool OSAtomicCompareAndSwapPtrBarrier(void *oldp, void *newp, void * volatile *dst)
 {
   return __sync_bool_compare_and_swap(dst, oldp, newp);
 }
 
-__inline BOOL OSAtomicCompareAndSwap32Barrier(int32_t oldl, int32_t newl, int32_t volatile *dst)
+static __inline bool OSAtomicCompareAndSwap32Barrier(int32_t oldl, int32_t newl, int32_t volatile *dst)
 {
   return __sync_bool_compare_and_swap(dst, oldl, newl);
 }
 
-__inline int32_t OSAtomicDecrement32(volatile int32_t *dst)
+static __inline int32_t OSAtomicDecrement32(volatile int32_t *dst)
 {
   return __sync_fetch_and_sub(dst, 1);
 }
 
-__inline int32_t OSAtomicDecrement32Barrier(volatile int32_t *dst)
+static __inline int32_t OSAtomicDecrement32Barrier(volatile int32_t *dst)
 {
   return __sync_fetch_and_sub(dst, 1);
 }
 
-__inline int32_t OSAtomicIncrement32(volatile int32_t *dst)
+static __inline int32_t OSAtomicIncrement32(volatile int32_t *dst)
 {
   return __sync_fetch_and_add(dst, 1);
 }
 
-__inline int32_t OSAtomicIncrement32Barrier(volatile int32_t *dst)
+static __inline int32_t OSAtomicIncrement32Barrier(volatile int32_t *dst)
 {
   return __sync_fetch_and_add(dst, 1);
 }
 
-__inline int32_t OSAtomicOr32(uint32_t theMask, volatile uint32_t *theValue)
+static __inline int32_t OSAtomicOr32(uint32_t theMask, volatile uint32_t *theValue)
 {
   return __sync_fetch_and_or(theValue, theMask);
 }
 
-__inline int32_t OSAtomicOr32Barrier(uint32_t theMask, volatile uint32_t *theValue)
+static __inline int32_t OSAtomicOr32Barrier(uint32_t theMask, volatile uint32_t *theValue)
 {
   return __sync_fetch_and_or(theValue, theMask);
 }
 
-__inline int32_t OSAtomicXor32(uint32_t theMask, volatile uint32_t *theValue)
+static __inline int32_t OSAtomicXor32(uint32_t theMask, volatile uint32_t *theValue)
 {
   return __sync_fetch_and_xor(theValue, theMask);
 }
 
-__inline int32_t OSAtomicXor32Barrier(uint32_t theMask, volatile uint32_t *theValue)
+static __inline int32_t OSAtomicXor32Barrier(uint32_t theMask, volatile uint32_t *theValue)
 {
   return __sync_fetch_and_xor(theValue, theMask);
 }
 
-__inline void OSMemoryBarrier() {
+static __inline void OSMemoryBarrier() {
 	    __sync_synchronize();
 }
 
@@ -81,7 +84,7 @@ inline void OSSpinLockUnlock(volatile OSSpinLock *lock) {
 	*lock = 0;
 }
 
-inline BOOL OSSpinLockTry(volatile OSSpinLock *lock) {
+inline bool OSSpinLockTry(volatile OSSpinLock *lock) {
 	return (__sync_val_compare_and_swap(lock, 0, ~0) == 0);
 }
 
