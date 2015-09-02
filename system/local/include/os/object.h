a5b4ceb8633776a8d919b71ad70aab845b63502a
Fumiya Chiba <fumiya.chiba@nifty.com>
2015-09-02 18:21:43 +0900

    Use objc/NSObject when compiling libdispatch

--- system/local/include/os/object.h
+++ system/local/include/os/object.h
@@ -72,7 +72,11 @@
 #endif
 
 #if OS_OBJECT_USE_OBJC
+#if __DISPATCH_BUILDING_DISPATCH__
 #import <Foundation/NSObject.h>
+#else
+#import <objc/NSObject.h>
+#endif
 #define OS_OBJECT_CLASS(name) OS_##name
 #define OS_OBJECT_DECL(name, ...) \
 		@protocol OS_OBJECT_CLASS(name) __VA_ARGS__ \
