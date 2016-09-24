2bc85323d4ce8d14e17065d881cf0aa362b50432
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2016-09-23 15:14:28 -1000

    Add syncfs templates

--- tombo/tombopffs/tombopffs.js
+++ tombo/tombopffs/tombopffs.js
@@ -3,14 +3,59 @@
 module.exports = {
   $TOMBOPFFS__deps: ['$FS', '$MEMFS', '$PATH'],
   $TOMBOPFFS: {
-    dbs: {},
     mount: function(mount) {
       // reuse all of the core MEMFS functionality
       return MEMFS.mount.apply(null, arguments);
     },
     syncfs: function(mount, populate, callback) {
-      // TODO: implement
-      callback(null);
+      TOMBOPFFS.getMEMFSEntries(mount, function(err, local) {
+        if (err) return callback(err);
+
+        TOMBOPFFS.getRemoteEntries(mount, function(err, local) {
+          if (err) return callback(err);
+
+          // TODO: implement
+          callback(null);
+        });
+      });
+    },
+    getMEMFSEntries: function(mount, callback) {
+      let entries = {};
+
+      function isRealDir(p) {
+        return p !== '.' && p !== '..';
+      };
+      function toAbsolute(root) {
+        return function(p) {
+          return PATH.join2(root, p);
+        }
+      };
+
+      let check = FS.readdir(mount.mountpoint).filter(isRealDir)
+                  .map(toAbsolute(mount.mountpoint));
+
+      while (check.length) {
+        const path = check.pop();
+        let stat;
+
+        try {
+          stat = FS.stat(path);
+        } catch (e) {
+          return callback(e);
+        }
+
+        if (FS.isDir(stat.mode)) {
+          check.push.apply(check, FS.readdir(path).filter(isRealDir).map(toAbsolute(path)));
+        }
+
+        entries[path] = { timestamp: stat.mtime };
+      }
+
+      return callback(null, { type: 'memfs', entries: entries });
+    },
+    getRemoteEntries: function(mount, callback) {
+      let entries = [];
+      return callback(null, { type: 'remote', entries: entries });
     }
   }
 };
