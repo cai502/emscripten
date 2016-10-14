0303f70f40dfd4d5bc9b8928ea7280770ed91874
Fumiya Chiba <fumiya.chiba@nifty.com>
2016-10-14 17:54:24 +0900

    Add sysctlbyname and related constants

--- system/include/compat/sys/sysctl.h
+++ system/include/compat/sys/sysctl.h
@@ -8,7 +8,11 @@ extern "C" {
 #define __NEED_size_t
 #include <bits/alltypes.h>
 
+#define CTL_KERN 1
+#define KERN_BOOTTIME 1
+
 int sysctl (int *, int, void *, size_t *, void *, size_t);
+int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 
 #ifdef __cplusplus
 }
