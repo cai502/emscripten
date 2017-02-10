b0c0b06b13b5f2152dc9b987750d610791eb2730
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-02-10 14:15:25 +0900

    Add http prefix to Module property

--- src/library_xhr.js
+++ src/library_xhr.js
@@ -14,7 +14,7 @@ var LibraryXHR = {
             return null;
         },
         useProxy: function(url) {
-            var prefixes = Module['proxyUrlPrefixes'] || [];
+            var prefixes = Module['httpProxyUrlPrefixes'] || [];
             for(var i = 0; i < prefixes.length; i++) {
                 var prefix = prefixes[i];
                 if(url.indexOf(prefix) == 0) return true;
@@ -43,7 +43,7 @@ var LibraryXHR = {
         xhr.useProxy = XHRWrapper.useProxy(xhr.url);
 
         if(xhr.useProxy) {
-            var proxyUrl = Module["proxyServer"] || "http://api.tombo.io/proxy";
+            var proxyUrl = Module["httpProxyServer"] || "http://api.tombo.io/proxy";
             xhr.open("POST", proxyUrl, xhr.async);
             xhr.headers = [];
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
