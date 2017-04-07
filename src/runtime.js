47f1ffebf64d8454348c65823421785a1f9941e7
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-04-07 11:52:39 +0900

    Revert "load async"
    This reverts commit a931ba669367a20cf76b37a08b933c4c80d7543a.

--- src/runtime.js
+++ src/runtime.js
@@ -320,37 +320,32 @@ var Runtime = {
   loadedDynamicLibraries: [],
 
   loadDynamicLibrary: function(lib) {
-    var exportModule = function(libModule) {
-      // add symbols into global namespace TODO: weak linking etc.
-      for (var sym in libModule) {
-        if (!Module.hasOwnProperty(sym)) {
-          Module[sym] = libModule[sym];
-        }
-#if ASSERTIONS == 2
-        else if (sym[0] === '_') {
-          var curr = Module[sym], next = libModule[sym];
-          // don't warn on functions - might be odr, linkonce_odr, etc.
-          if (!(typeof curr === 'function' && typeof next === 'function')) {
-            Module.printErr("warning: trying to dynamically load symbol '" + sym + "' (from '" + lib + "') that already exists (duplicate symbol? or weak linking, which isn't supported yet?)"); // + [curr, ' vs ', next]);
-          }
-        }
-#endif
-      }
-      Runtime.loadedDynamicLibraries.push(libModule);
-    };
 #if BINARYEN
-    Module['readAsync'](lib, function(bin) {
-      var libModule = Runtime.loadWebAssemblyModule(new Uint8Array(bin));
-      exportModule(libModule);
-    }, function(){});
+    var bin = Module['readBinary'](lib);
+    var libModule = Runtime.loadWebAssemblyModule(bin);
 #else
     var src = Module['read'](lib);
     var libModule = eval(src)(
       Runtime.alignFunctionTables(),
       Module
     );
-    exportModule(libModule);
 #endif
+    // add symbols into global namespace TODO: weak linking etc.
+    for (var sym in libModule) {
+      if (!Module.hasOwnProperty(sym)) {
+        Module[sym] = libModule[sym];
+      }
+#if ASSERTIONS == 2
+      else if (sym[0] === '_') {
+        var curr = Module[sym], next = libModule[sym];
+        // don't warn on functions - might be odr, linkonce_odr, etc.
+        if (!(typeof curr === 'function' && typeof next === 'function')) {
+          Module.printErr("warning: trying to dynamically load symbol '" + sym + "' (from '" + lib + "') that already exists (duplicate symbol? or weak linking, which isn't supported yet?)"); // + [curr, ' vs ', next]);
+        }
+      }
+#endif
+    }
+    Runtime.loadedDynamicLibraries.push(libModule);
   },
 
 #if BINARYEN
@@ -660,3 +655,4 @@ if (RETAIN_COMPILER_SETTINGS) {
     } catch(e){}
   }
 }
+
