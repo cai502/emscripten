7207ec87f4cb2520e3d651cb55e4abdd68f7cf0f
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2017-04-16 05:00:18 -0700

    Call AWS CLI with the credentials written in a file

--- tests/test_tombo.py
+++ tests/test_tombo.py
@@ -1,6 +1,7 @@
 import BaseHTTPServer, multiprocessing, os, shutil, subprocess, unittest, zlib, webbrowser, time, shlex
 from runner import BrowserCore, path_from_root
 from tools.shared import *
+import ConfigParser # for reading AWS credentials
 
 # User can specify an environment variable EMSCRIPTEN_BROWSER to force the browser test suite to
 # run using another browser command line than the default system browser.
@@ -16,11 +17,26 @@ if emscripten_browser:
 class tombo(BrowserCore):
   @classmethod
   def setUpClass(self):
+    self.initialize_aws_s3()
     super(tombo, self).setUpClass()
     self.browser_timeout = 20
     print
     print 'Running the browser tests. Make sure the browser allows popups from localhost.'
     print
+
+  @classmethod
+  def initialize_aws_s3(self):
+    aws_credentials_path = './tombo/aws_credentials'
+    if not os.path.exists(aws_credentials_path):
+      raise 'Cannot find {0}'.format(aws_credentails_path)
+    p = ConfigParser.SafeConfigParser()
+    p.read('./tombo/aws_credentials')
+    aws_access_key_id = p.get('default', 'aws_access_key_id')
+    aws_secret_access_key = p.get('default', 'aws_secret_access_key')
+    aws_env = os.environ.copy()
+    aws_env['AWS_ACCESS_KEY_ID'] = aws_access_key_id
+    aws_env['AWS_SECRET_ACCESS_KEY'] = aws_secret_access_key
+    Popen(['aws', 's3', 'ls', '--region=us-west-2', '--output=json'], env=aws_env)
 
   def test_fs_tombofs_sync(self):
     for extra in [[], ['-DEXTRA_WORK']]:
