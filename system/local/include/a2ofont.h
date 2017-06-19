121af494f2bd75281aa6cd0c473d5d1faa87c62c
Fumiya Chiba <fumiya.chiba@nifty.com>
2017-06-20 00:46:24 +0900

    Add include

--- system/local/include/a2ofont.h
+++ system/local/include/a2ofont.h
@@ -1,3 +1,5 @@
+#include <inttypes.h>
+
 extern uint8_t *a2o_renderFontToBitmapBuffer(const char* fontName, int pointSize, const char* str, float a, float b, float c, float d, int *width, int *height, int *left, int *top);
 extern void a2o_getFontMetrics(const char* fontName, int pointSize, int32_t *ascent, int32_t *descent, int32_t *capHeight, int32_t *xHeight);
 extern int a2o_getTextWidth(const char* fontName,  int pointSize, const char* str);
